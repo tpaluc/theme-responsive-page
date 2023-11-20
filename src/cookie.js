193873265d299e2f027c17c5bd3fb9699ab60e1a
tomek <paluch94@gmail.com>
2023-11-20 15:04:51 +0100

    cookie

--- src/cookie.js
+++ src/cookie.js
@@ -5,7 +5,7 @@ tl.fromTo(".cookie", {opacity:0, x: -50, rotation: '-45deg'}, {opacity: 1, x: 0,
 tl.fromTo(".cookie__text", {opacity: 0, x: 30}, {opacity: 1, x:0}, "<")
 
 tl.fromTo('.cookie', {y:0, rotation: '0deg'}, {y:-20, rotation: '-20deg', yoyo: true, repeat: -1})
-tl.fromTo('#crumbs', {y:0}, {y:-100, yoyo: true, repeat: -1},"<")
+tl.fromTo('#crumbs', {y:0}, {y:-150, yoyo: true, repeat: -1},"<")
 
 const button = document.querySelector(".btn--cookie");
 
